30e812cb7751b6a17e62e1922baa10ab7e51a0b2
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-06-01 18:11:34 -0300

    Created the component Header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import {Header} from "./components/Header";
+
 import './styles/global.scss'
 
 export function App() {
 	return (
 		<>
+			<Header />
 			<h1>Hello World!</h1>
 			<p>Bazinga!!!</p>
 		</>
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import '../styles/header.scss'
+
+export function Header() {
+	return (
+		<header className="header">
+			<div>
+				<img src="/logo.svg" alt="logo of the project To Do List." />
+			</div>
+		</header>
+	)
+}
